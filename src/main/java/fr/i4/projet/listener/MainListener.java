a04bca7cf226fec99bbe09fdbb309f779d570013
MhD <mehdi.mahmal@epsi.fr>
2015-01-28 15:35:21 +0100

    Update Servlet Produit et Listenet MainListener initialisation Liste des Produits

--- src/main/java/fr/i4/projet/listener/MainListener.java
+++ src/main/java/fr/i4/projet/listener/MainListener.java
@@ -1,12 +1,51 @@
 package fr.i4.projet.listener;
 
+import fr.i4.projet.bean.Produit;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by maxoumime on 28/01/15.
  */
 public class MainListener implements ServletContextListener {
+
+    private static List<Produit> listProduits;
+
+    public static List<Produit> getListProduits() {
+        return listProduits;
+    }
+
+    public MainListener() {
+        listProduits = new ArrayList<Produit>();
+        Produit produit = new Produit();
+        produit.setReference("1EE3F");
+        produit.setNom("Table Rouge");
+        produit.setQuantity(10);
+        listProduits.add(produit);
+
+        produit = new Produit();
+        produit.setReference("8HD8J");
+        produit.setNom("Table Verte");
+        produit.setQuantity(5);
+        listProduits.add(produit);
+
+        produit = new Produit();
+        produit.setReference("9IK7J");
+        produit.setNom("Chaise Bleu");
+        produit.setQuantity(8);
+        listProduits.add(produit);
+
+        produit = new Produit();
+        produit.setReference("0KS6T");
+        produit.setNom("Chaise Jaune");
+        produit.setQuantity(2);
+        listProduits.add(produit);
+
+    }
+
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
 
--- src/main/java/fr/i4/projet/servlet/ProduitServlet.java
+++ src/main/java/fr/i4/projet/servlet/ProduitServlet.java
@@ -1,6 +1,11 @@
 package fr.i4.projet.servlet;
 
 
+import fr.i4.projet.bean.Produit;
+import fr.i4.projet.listener.MainListener;
+import fr.i4.projet.service.ProduitService;
+import fr.i4.projet.service.impl.ProduitServiceImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,8 +27,12 @@ public class ProduitServlet extends HttpServlet {
         String reference = request.getParameter("reference");
         String quantityOrdered = request.getParameter("quantityOrdered");
 
-        if(quantityOrdered != null && reference != null){
-
+        if(Integer.parseInt(quantityOrdered) != 0 && reference != null){
+            for (Produit produit : MainListener.getListProduits()) {
+                if(produit.getQuantity() >= Integer.parseInt(quantityOrdered)){
+                    produit.setQuantity((produit.getQuantity() - Integer.parseInt(quantityOrdered)));
+                }
+            }
         }
 
 
