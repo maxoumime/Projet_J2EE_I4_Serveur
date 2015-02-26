e4411a16c6e7b265e3361e627de6ce6190e6606c
MhD <mehdi.mahmal@epsi.fr>
2015-02-26 14:13:03 +0100

    UPDATE, Angular Config : - Ajout Factory de Récupération Liste de Produits dans produitService.js

--- WebContent/js/produits/produitController.js
+++ WebContent/js/produits/produitController.js
@@ -1,3 +1,7 @@
 /**
  * Created by MhD on 26/02/15.
  */
+angular.module('app.controllers', []).
+    controller('produitController', function($scope) {
+
+    });
--- src/main/java/fr/i4/projet/controller/rest/ProduitController.java
+++ src/main/java/fr/i4/projet/controller/rest/ProduitController.java
@@ -3,6 +3,7 @@ package fr.i4.projet.controller.rest;
 import fr.i4.projet.bean.Produit;
 import fr.i4.projet.service.ProduitService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +18,7 @@ import java.util.List;
 public class ProduitController {
 
 
+    @Qualifier("produit")
     @Autowired
     private ProduitService produitService;
 
