1d6773bf0a52a87001e8541a72079732495930b9
JDEWILDE <jdewilde@epsi.fr>
2015-03-09 15:31:40 +0100

    JDWD: Mise en place de la vue de consultation des produits

--- WebContent/js/produits/produitController.js
+++ WebContent/js/produits/produitController.js
@@ -5,10 +5,11 @@
 var produitModule = angular.module('produit.module', ['EPSIapp']);
 
 produitModule.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/produit',    {templateUrl: 'templates/produitView.html', controller: 'ProduitCtrl'});
+    $routeProvider.when('/produit',    {templateUrl: 'templates/productDisplay.html', controller: 'ProduitCtrl'});
 }]);
 
 produitModule.controller('ProduitCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
 
-    $scope.produits = "Hey";
+    $scope.products = [{"nom":"Table Rouge","reference":"1EE3F","quantity":10},{"nom":"Table Verte","reference":"8HD8J","quantity":5},{"nom":"Chaise Bleu","reference":"9IK7J","quantity":8},{"nom":"Chaise Jaune","reference":"0KS6T","quantity":2}];
+    
 }]);
